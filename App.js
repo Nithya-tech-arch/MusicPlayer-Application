529431ebf551196aa9a2c9082897bb85a250b8e8
Nithyasree <nithya2426845@gmail.com>
2021-10-31 22:29:52 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
+
+import MusicPlayer from './component/MusicPlayer';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const App=()=>{
+  return(
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content"/>
+      <MusicPlayer/>
+    </View>
+  );
+};
+
+export default App;
+const styles=StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+});
